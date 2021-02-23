0c4506f9467dedf03467d501cab6f941ee15dbbb
sudaojun <1251662462@qq.com>
2021-02-23 15:43:51 +0800

    角色删除提示信息

--- src/views/user/roleList.vue
+++ src/views/user/roleList.vue
@@ -581,7 +581,7 @@ export default {
       }
     },
     dataDel(params){
-      this.$confirm('此操作将删除该用户信息, 是否继续?', '提示', {
+      this.$confirm('此操作将删除该角色信息, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
